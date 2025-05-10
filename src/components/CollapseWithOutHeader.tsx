99dd094a8b90274088b9d7095d81ecc52bd299dc
zejba <66516617+zejba@users.noreply.github.com>
2025-05-11 01:20:57 +0900

    feat: CollapseWithoutHeader

---
+++ src/components/CollapseWithOutHeader.tsx
@@ -0,0 +1,34 @@
+import { Collapse } from 'antd'
+import styled from 'styled-components'
+
+const StyledCollapse = styled(Collapse)`
+  .ant-collapse-header {
+    display: none !important;
+  }
+  .ant-collapse-content-box {
+    padding: 0 !important;
+  }
+`
+
+interface Props {
+	isActive: boolean
+	children: React.ReactNode
+}
+
+function CollapseWithOutHeader(props: Props) {
+	const { isActive, children } = props
+	return (
+		<StyledCollapse
+			ghost
+			activeKey={isActive ? ['collapse'] : []}
+			items={[
+				{
+					key: 'collapse',
+					children: children,
+				},
+			]}
+		/>
+	)
+}
+
+export default CollapseWithOutHeader
--- src/features/DamageCalculator/BaseInfoNpStarSection.tsx
+++ src/features/DamageCalculator/BaseInfoNpStarSection.tsx
@@ -1,26 +1,9 @@
-import {
-	Collapse,
-	Flex,
-	Form,
-	InputNumber,
-	Space,
-	Switch,
-	Typography,
-} from 'antd'
+import { Flex, Form, InputNumber, Space, Switch, Typography } from 'antd'
 import { useAtom } from 'jotai'
 import { useCallback } from 'react'
-import styled from 'styled-components'
+import CollapseWithOutHeader from '../../components/CollapseWithOutHeader'
 import SpaceCompactHeader from '../../components/SpaceCompactHeader'
 import { isRequiredNpStarCalcAtom } from './jotai'
-
-const StyledCollapse = styled(Collapse)`
-  .ant-collapse-header {
-    display: none !important;
-  }
-  .ant-collapse-content-box {
-    padding: 0 !important;
-  }
-`
 
 function BaseInfoNpStarSection() {
 	const [isRequiredNpStarCalc, setIsRequiredNpStarCalc] = useAtom(
@@ -38,73 +21,64 @@ function BaseInfoNpStarSection() {
 					onChange={toggleIsRequiredNpStarCalc}
 				/>
 			</Space>
-			<StyledCollapse
-				ghost
-				activeKey={isRequiredNpStarCalc ? ['npStar'] : []}
-				items={[
-					{
-						key: 'npStar',
-						children: (
-							<Flex vertical gap={12} align="flex-start">
-								<Form.Item name="npGain">
-									<InputNumber
-										addonBefore="NP獲得量"
-										addonAfter="%"
-										type="number"
-										style={{ width: 180 }}
-									/>
-								</Form.Item>
-								<Form.Item name="starRate">
-									<InputNumber
-										addonBefore="スター発生率"
-										addonAfter="%"
-										type="number"
-										style={{ width: 200 }}
-									/>
-								</Form.Item>
-								<Space.Compact>
-									<SpaceCompactHeader>Hit数</SpaceCompactHeader>
-									<Form.Item name="hitCountN">
-										<InputNumber
-											type="number"
-											addonBefore="N"
-											style={{ width: 80 }}
-										/>
-									</Form.Item>
-									<Form.Item name="hitCountB">
-										<InputNumber
-											type="number"
-											addonBefore="B"
-											style={{ width: 80 }}
-										/>
-									</Form.Item>
-									<Form.Item name="hitCountA">
-										<InputNumber
-											type="number"
-											addonBefore="A"
-											style={{ width: 80 }}
-										/>
-									</Form.Item>
-									<Form.Item name="hitCountQ">
-										<InputNumber
-											type="number"
-											addonBefore="Q"
-											style={{ width: 80 }}
-										/>
-									</Form.Item>
-									<Form.Item name="hitCountEX">
-										<InputNumber
-											type="number"
-											addonBefore="EX"
-											style={{ width: 80 }}
-										/>
-									</Form.Item>
-								</Space.Compact>
-							</Flex>
-						),
-					},
-				]}
-			/>
+			<CollapseWithOutHeader isActive={isRequiredNpStarCalc}>
+				<Flex vertical gap={12} align="flex-start">
+					<Form.Item name="npGain">
+						<InputNumber
+							addonBefore="NP獲得量"
+							addonAfter="%"
+							type="number"
+							style={{ width: 180 }}
+						/>
+					</Form.Item>
+					<Form.Item name="starRate">
+						<InputNumber
+							addonBefore="スター発生率"
+							addonAfter="%"
+							type="number"
+							style={{ width: 200 }}
+						/>
+					</Form.Item>
+					<Space.Compact>
+						<SpaceCompactHeader>Hit数</SpaceCompactHeader>
+						<Form.Item name="hitCountN">
+							<InputNumber
+								type="number"
+								addonBefore="N"
+								style={{ width: 80 }}
+							/>
+						</Form.Item>
+						<Form.Item name="hitCountB">
+							<InputNumber
+								type="number"
+								addonBefore="B"
+								style={{ width: 80 }}
+							/>
+						</Form.Item>
+						<Form.Item name="hitCountA">
+							<InputNumber
+								type="number"
+								addonBefore="A"
+								style={{ width: 80 }}
+							/>
+						</Form.Item>
+						<Form.Item name="hitCountQ">
+							<InputNumber
+								type="number"
+								addonBefore="Q"
+								style={{ width: 80 }}
+							/>
+						</Form.Item>
+						<Form.Item name="hitCountEX">
+							<InputNumber
+								type="number"
+								addonBefore="EX"
+								style={{ width: 80 }}
+							/>
+						</Form.Item>
+					</Space.Compact>
+				</Flex>
+			</CollapseWithOutHeader>
 		</>
 	)
 }
